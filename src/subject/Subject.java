efde2f5f35e418b946a75616bb57f3dc8d3edcb5
yuka0223 <kmm2459339@stu.o-hara.ac.jp>
2025-04-23 11:25:08 +0900

    Subject.javaを追加

---
+++ src/subject/Subject.java
@@ -0,0 +1,22 @@
+package subject;
+
+public class Subject {
+
+    private String code;
+
+    private String name;
+
+    public Subject(String code, String name) {
+
+        this.code = code;
+
+        this.name = name;
+
+    }
+
+    public String getCode() { return code; }
+
+    public String getName() { return name; }
+
+}
+
